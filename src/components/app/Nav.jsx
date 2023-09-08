d9adbf8777064d6b767be6fe4534c1d698143953
floreesleo <florees.leo@gmail.com>
2023-09-08 16:47:04 -0600

    primeros estilos css en Nav

--- src/components/app/Nav.jsx
+++ src/components/app/Nav.jsx
@@ -3,26 +3,28 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAustralSign } from "@fortawesome/free-solid-svg-icons";
 
+import "./../../assets/css/nav.css";
+
 export default function NavBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" bg="dark">
+    <Navbar expand="lg" className="navegador">
       <Container>
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/" className="titulo">
           <FontAwesomeIcon icon={faAustralSign} /> Colegio de arquitectos
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
+          <Nav className="me-auto texto">
+            <Nav.Link as={Link} to="/" className="texto">
               Inicio
             </Nav.Link>
-            <Nav.Link as={Link} to="/nosotros">
+            <Nav.Link as={Link} to="/nosotros" className="texto">
               Nosotros
             </Nav.Link>
-            <Nav.Link as={Link} to="/archivos">
+            <Nav.Link as={Link} to="/archivos" className="texto">
               Documentos
             </Nav.Link>
-            <Nav.Link as={Link} to="/perfil">
+            <Nav.Link as={Link} to="/perfil" className="texto">
               Perfil
             </Nav.Link>
           </Nav>
