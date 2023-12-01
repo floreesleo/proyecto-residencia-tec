694b4df23d59475cfef3004b317b6082063a72de
floreesleo <florees.leo@gmail.com>
2023-11-30 18:57:23 -0600

    style: Carrusel
    Cambio en un carrusel innecesario

--- src/components/app/home/Carrusel.jsx
+++ src/components/app/home/Carrusel.jsx
@@ -3,7 +3,6 @@ import { Carousel, Figure } from "react-bootstrap";
 
 // Imagenes de prueba
 import fondoColegio from "./../../../assets/img/fondo-colegio.png";
-import example2 from "./../../../assets/img/example-2.jpg";
 import example3 from "./../../../assets/img/example-3.jpg";
 
 export default function Carrusel() {
@@ -18,20 +17,6 @@ export default function Carrusel() {
           <p style={{ color: "#000" }}>
             Celebra con nosotros el lanzamiento de la nueva aplicación del
             Colegio de arquitectos de Comitán
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Figure>
-          <Figure.Image src={example2} />
-        </Figure>
-        <Carousel.Caption>
-          <h3 style={{ color: "#000" }}>Nuestra misión</h3>
-          <p style={{ color: "#000" }}>
-            Nuestra misión es integrar un grupo unido; con profesionales de la
-            Arquitectura, comprometidos con nuestra profesión; preocupados por
-            nuestros colegas y todas aquellas instituciones o personas que
-            dependan de nuestro servicio.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
